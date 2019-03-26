acf1c0dda80986e5c6005350c8ad4ee00d333eda
Daniel D'Avella <ddavella@stsci.edu>
2019-03-26 10:59:08 -0400

    Add custom footer to schema docs

--- sphinx_asdf/__init__.py
+++ sphinx_asdf/__init__.py
@@ -2,8 +2,9 @@ import os
 
 from .nodes import add_asdf_nodes
 from .directives import AsdfAutoschemas, AsdfSchema, schema_def
-from .connections import (autogenerate_schema_docs, handle_page_context,
-                          add_labels_to_nodes, on_build_finished)
+from .connections import (autogenerate_schema_docs, update_app_config,
+                          handle_page_context, add_labels_to_nodes,
+                          on_build_finished)
 
 from sphinx.builders.html import StandaloneHTMLBuilder
 
@@ -24,6 +25,7 @@ def setup(app):
     app.add_js_file('sphinx_asdf.js')
 
     app.connect('builder-inited', autogenerate_schema_docs)
+    app.connect('config-inited', update_app_config)
     app.connect('html-page-context', handle_page_context)
     app.connect('doctree-read', add_labels_to_nodes)
     app.connect('build-finished', on_build_finished)
