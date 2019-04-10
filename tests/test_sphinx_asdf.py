78e03be749b28bc02e255109ed67e941e462503c
Daniel D'Avella <ddavella@stsci.edu>
2019-04-10 16:32:36 -0400

    Add test for doc generation with asdf_schema_path

--- tests/test_sphinx_asdf.py
+++ tests/test_sphinx_asdf.py
@@ -45,3 +45,29 @@ def test_basic_generation(app, status, warning):
             '    :schema_root: schemas\n'
             '\n'
             '    core/baz\n') in baz_doc.text()
+
+
+@pytest.mark.sphinx('dummy', testroot='global-config')
+def test_generation_global_config(app, status, warning):
+    app.builder.build_all()
+
+    foo_doc = (app.srcdir / 'generated' / 'foo.rst')
+    assert foo_doc.exists()
+    assert ('.. asdf-schema::\n'
+            '    :schema_root: a/b/c/schemas\n'
+            '\n'
+            '    foo\n') in foo_doc.text()
+
+    bar_doc = (app.srcdir / 'generated' / 'bar.rst')
+    assert bar_doc.exists()
+    assert ('.. asdf-schema::\n'
+            '    :schema_root: a/b/c/schemas\n'
+            '\n'
+            '    bar\n') in bar_doc.text()
+
+    baz_doc = (app.srcdir / 'generated' / 'core' / 'baz.rst')
+    assert baz_doc.exists()
+    assert ('.. asdf-schema::\n'
+            '    :schema_root: a/b/c/schemas\n'
+            '\n'
+            '    core/baz\n') in baz_doc.text()
